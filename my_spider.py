850a1ff5bf947efeef26db8add825c720e0ea3b2
liming <39261724+xiaoxlm@users.noreply.github.com>
2021-09-19 22:43:53 +0800

    echo data

--- my_spider.py
+++ my_spider.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+# WebElement
+from selenium.webdriver.remote.webelement import WebElement
+
+
+def echoElement(ele:WebElement):
+    text = ele.text
+    data = text.split(" ")
+    print(count, ":", data[1], "|", data[6])
+
+
 if __name__ == '__main__':
     chrome_options = Options()
     # chrome_options.add_argument("--headless")
@@ -11,12 +21,14 @@ if __name__ == '__main__':
     # data = driver.page_source   #获取网页文本
     print(driver.title)
     try:
-        element = driver.find_elements_by_xpath("//table[@class='m-table J-ajax-table']/tbody/tr/td[@class='tl']/a")
+        # element = driver.find_elements_by_xpath("//table[@class='m-table J-ajax-table']/tbody/tr/td[@class='tl']/a")
+        element = driver.find_elements_by_xpath("//table[@class='m-table J-ajax-table']/tbody/tr")
         count = 0
 
         for i in element:
             count += 1
-            print(count, ":", i.accessible_name)
+            # print(count, ":", i.accessible_name)
+            echoElement(i)
     except Exception as e:
         print(e)
 
